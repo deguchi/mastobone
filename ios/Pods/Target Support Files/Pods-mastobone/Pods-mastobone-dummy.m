e73fc1f66a60558bdc31eae32b285e73caf0f7dd
Satoshi Deguchi <deguchik@gmail.com>
2017-08-08 10:16:39 +0900

    pod init and pod install

---
+++ ios/Pods/Target Support Files/Pods-mastobone/Pods-mastobone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mastobone : NSObject
+@end
+@implementation PodsDummy_Pods_mastobone
+@end
---
+++ ios/Pods/Target Support Files/Pods-mastoboneTests/Pods-mastoboneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mastoboneTests : NSObject
+@end
+@implementation PodsDummy_Pods_mastoboneTests
+@end
